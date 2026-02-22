660ece9db0e541d2fa3df6a9599812c594408a1c
aakashsingghh <akashsingh33280@gmail.com>
2026-02-22 23:53:26 +0530

    Defining Logic

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,22 @@
 #define trigPin 9
 #define echoPin 10
 
-// void setup() {
-//     Serial.begin(9600);
-//     pinMode(trigPin, OUTPUT);
-//     pinMode(echoPin, INPUT);
-// }
-//
-// void loop() {
-//     digitalWrite(trigPin, LOW);
-//     delayMicroseconds(2);
-//     digitalWrite(trigPin, HIGH);
-//     delayMicroseconds(10);
-//     digitalWrite(trigPin, LOW);
-//     long duration = pulseIn(echoPin, HIGH);
-//     long distance = duration * 0.034 / 2;
-//     Serial.println("distance : ");
-//     Serial.println(distance);
-//     Serial.println(" cm");
-//     delay(1000);
-// }
+void setup() {
+    Serial.begin(9600);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.034 / 2;
+    Serial.println("distance : ");
+    Serial.println(distance);
+    Serial.println(" cm");
+    delay(1000);
+}
