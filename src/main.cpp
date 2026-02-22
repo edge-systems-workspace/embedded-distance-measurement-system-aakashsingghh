3b1cbefc4661845b30bd15a5345ec5144e5fc430
aakashsingghh <akashsingh33280@gmail.com>
2026-02-22 23:58:39 +0530

    Logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 int touchPin=7;
 int ledPin=13;
 
-// void setup() {
-//     pinMode(touchPin, INPUT);
-//     pinMode(ledPin, OUTPUT);
-//     Serial.begin(9600);
-// }
-//
-// void loop() {
-//     int state=digitalRead(touchPin);
-//     if(state==1){
-//         digitalWrite(ledPin, 1);
-//         Serial.println("Touched!!");
-//     }
-//     else{
-//         digitalWrite(ledPin, 0);
-//         Serial.println("Not Touched!!");
-//
-//
-//     }
-//
-// }
+void setup() {
+    pinMode(touchPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    int state=digitalRead(touchPin);
+    if(state==1){
+        digitalWrite(ledPin, 1);
+        Serial.println("Touched!!");
+    }
+    else{
+        digitalWrite(ledPin, 0);
+        Serial.println("Not Touched!!");
+
+
+    }
+
+}
